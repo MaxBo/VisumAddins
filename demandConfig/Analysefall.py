124f69426a8ddc9a12221735a8e336644b7b366f
kohnenn <kohnen@ggr-planung.de>
2013-09-18 19:26:25 +0200

    config added

---
+++ demandConfig/Analysefall.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Barcelona
+#
+# Created:     24.04.2013
+# Copyright:   (c) Barcelona 2013
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+self.openPut.SetValue('Base0.h5')
+self.openPrt.SetValue('SkimsPrT.h5')
+self.openNmt.SetValue('SkimsNmT.h5')
+self.openStruktur.SetValue('zonal_2010.h5')
