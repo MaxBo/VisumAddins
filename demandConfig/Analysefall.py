755a7626de6204fd8e98b903b0bab106bb4e59cf
unknown <MaxBohnet@BARCELONA-PC.GGR.local>
2013-10-27 00:21:40 +0200

    default Analysefall changed

--- demandConfig/Analysefall.py
+++ demandConfig/Analysefall.py
@@ -9,8 +9,8 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
-self.openPut.SetValue('Base0.h5')
-self.openPrt.SetValue('SkimsPrT.h5')
+self.openPut.SetValue('NF2010_FZK6.h5')
+self.openPrt.SetValue('NF2010_FZK.h5')
 self.openNmt.SetValue('SkimsNmT.h5')
 self.openStruktur.SetValue('zonal_2010.h5')
 self.openParams.SetValue('params.h5')
