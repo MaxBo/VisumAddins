51a25bb1a7e2d4a3f84ca6d1d80ec69660ad7eee
MaxBo <max.bohnet@tuhh.de>
2016-11-20 11:58:38 +0100

    fix error in destination parking time, add progress dialog

--- VISEM_Kiel/preprocess_parking_body.py
+++ VISEM_Kiel/preprocess_parking_body.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
 
 
+import sys
+
+if __package__ is None:
+    from os import path
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
 import numpy as np
+from helpers.visumpy_with_progress_dialog import AddIn, AddInState
 
 
-def main():
+def main(Visum, addIn):
     """"""
     code = 'VisemT'
     dm = Visum.Net.DemandModels.ItemByKey(code)
 
-    add_parking_by_activity(dm)
+    add_parking_by_activity(dm, addIn)
 
 
 def add_parking_by_activity(dm, skim='PARKING', home='W'):
@@ -19,13 +26,22 @@ def add_parking_by_activity(dm, skim='PARKING', home='W'):
     park_typ = np.array(p).ravel().astype('i4')
     different_park_zones, park_id = np.unique(park_typ, return_inverse=True)
     n_park_zones = len(different_park_zones)
+    
     park_costs_home = np.zeros((n_park_zones), dtype='d')
     park_costs = np.zeros((n_park_zones), dtype='d')
     activities = dm.Activities
     acts = activities.GetAll
-
+    n_activities = len(acts)
+    
+    addIn.ShowProgressDialog("Calculate Parking Matrices for {N} Activities".format(N=n_activities).decode("iso-8859-15"),
+                             "Calculate Parking Matrices", n_activities, setTimeMode=True)
+    i = 1
     for act in acts:
         if act.AttValue('IsHomeActivity'):
+            if addIn.ExecutionCanceled:
+                raise RuntimeError('Aborted at DS {i}'.format(i=i))       
+            addIn.UpdateProgressDialog(i, 'Calculate Parking at Home'.decode("iso-8859-15"))
+            
             for z, p in enumerate(different_park_zones):
                 park_costs_home[z] = \
                     act.AttValue('PARKING_ZONE_{p}'.format(p=p))
@@ -34,11 +50,17 @@ def add_parking_by_activity(dm, skim='PARKING', home='W'):
 
     for act in acts:
         if not act.AttValue('IsHomeActivity'):
+            i += 1
             a_code = act.AttValue('Code')
             a_name = act.AttValue('Name')
+            if addIn.ExecutionCanceled:
+                raise RuntimeError('Aborted at DS {i}'.format(i=i))       
+            addIn.UpdateProgressDialog(i, u'Calculate Matrix PARKING_{a} for {b}'.format(
+                a=a_code, b=a_name))
+            
             for z, p in enumerate(different_park_zones):
                 park_costs[z] = act.AttValue('PARKING_ZONE_{p}'.format(p=p))
-            costs_at_destination = np.take(park_costs, park_id)
+                costs_at_destination = np.take(park_costs, park_id)
             total_costs = (costs_at_homezone + costs_at_destination) / 2
             ref = 'MATRIX([CODE] = "PARKING_{a}")'.format(a=a_code)
             try:
@@ -47,6 +69,22 @@ def add_parking_by_activity(dm, skim='PARKING', home='W'):
                 raise ValueError(ref)
             m.SetValues(total_costs)
 
+    addIn.ReportMessage('calculated Parking Matrices for {i} activities'.format(i=i).decode("iso-8859-15"),
+                        messageType=2)
+    addIn.UpdateProgressDialog(n_activities)
+    addIn.CloseProgressDialog()
+
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) > 1:
+        addIn = AddIn()
+    else:
+        addIn = AddIn(Visum)    
+        
+    if addIn.State != AddInState.OK:
+        addIn.ReportMessage(addIn.ErrorObjects[0].ErrorMessage)
+    else:
+        try:            
+            main(Visum, addIn)
+        except:
+            addIn.HandleException(addIn.TemplateText.MainApplicationError)
