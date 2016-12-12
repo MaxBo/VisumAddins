719a3b71030b23f6d22fafdf3712b36f2c085a28
MaxBo <max.bohnet@tuhh.de>
2016-12-12 10:35:07 +0100

    Progress Dialog in SetCapacityFactorCR and limit to 53 CR-Fuctions

--- VISEM_Kiel/set_capacity_factor_cr_functions.py
+++ VISEM_Kiel/set_capacity_factor_cr_functions.py
@@ -1,21 +1,55 @@
 # -*- coding: utf-8 -*-
 
 from argparse import ArgumentParser
+import sys
+
+if __package__ is None:
+    from os import path
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+from helpers.visumpy_with_progress_dialog import AddIn, AddInState
 
 
-def main(Visum, capacityFactor):
+def main(Visum, capacityFactor, addIn):
     """"""
     crs = Visum.Procedures.Functions.CRFunctions
+    n_cr_functions = 54
     
-    for i in range(1, 100):
+    addIn.ShowProgressDialog(
+        u"Set CR Capacity Factors",
+        u"Set CR Capacity Factors", n_cr_functions, setTimeMode=True)
+    for i in range(1, n_cr_functions):
+        if addIn.ExecutionCanceled:
+            raise RuntimeError(u'Aborted at Linktype {i}'.format(i=i))       
         cr = crs.CrFunction(i)
         cr.SetAttValue('capacityFactor', capacityFactor)
+        addIn.UpdateProgressDialog(
+            i, u'"Set CR Capacity Factor for linktype {i}'.format(i=i))
+    addIn.UpdateProgressDialog(n_cr_functions)
+    addIn.CloseProgressDialog()
+    addIn.ReportMessage(u'Capacity Factors set for {i} linktypes'.format(i=i),
+                        messageType=2)
         
 
 
 if __name__ == '__main__':
     argparse = ArgumentParser()
     argparse.add_argument('-f', help='CapacityFactor',
-                          dest='capacityFactor', default=9.0)
+                          dest='capacityFactor', default=11.0)
     options = argparse.parse_args()
-    main(Visum, options.capacityFactor)
+
+    if len(sys.argv) > 1:
+        addIn = AddIn()
+    else:
+        addIn = AddIn(Visum)    
+        
+    if addIn.State != AddInState.OK:
+        addIn.ReportMessage(addIn.ErrorObjects[0].ErrorMessage)
+    else:
+        try:            
+            main(Visum, options.capacityFactor, addIn)
+        except:
+            addIn.HandleException(addIn.TemplateText.MainApplicationError)
+
+
+    
