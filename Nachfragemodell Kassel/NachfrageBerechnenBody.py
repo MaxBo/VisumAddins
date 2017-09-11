cbe3527f6a4137e97e9dcd0b041801ff3a87f6dc
MaxBo <bohnet@ggr-planung.de>
2017-09-11 15:29:22 +0200

    Anführungszeichen für Szenario in NachfrageBerechnen

--- Nachfragemodell Kassel/NachfrageBerechnenBody.py
+++ Nachfragemodell Kassel/NachfrageBerechnenBody.py
@@ -30,13 +30,12 @@ class CalcDemand(object):
 
     def execute(self):
         project_xml_file = os.path.join(self.project_folder, 'project.xml')
-        cmd = '"{pythonpath}" -m gui_vm.main -o "{project_xml_file}" -s "{scenario_name}" --run-specific {run}{balancing}'
+        cmd = '"{pythonpath}" -m gui_vm.main -o "{project_xml_file}" -s "{scenario_name}" --run-specific "{run}" {balancing}'
         full_cmd = cmd.format(pythonpath=self.pythonpath,
-                                        project_xml_file=project_xml_file,
-                                        scenario_name='"{}"'.format(
-                                            self.scenario_name),
-                                        run='"{}"'.format(self.run_name),
-                                        balancing=' --balancing')
+                              project_xml_file=project_xml_file,
+                              scenario_name=self.scenario_name,
+                              run=self.run_name,
+                              balancing=' --balancing')
         c = subprocess.Popen(full_cmd,
                              stdout=subprocess.PIPE,
                              stderr=subprocess.STDOUT,
